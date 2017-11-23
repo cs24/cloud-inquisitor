cefadcf99de5fad87db61dbe2c1ea42d1cb7887a
Reza Nikoopour <rnikoopour@riotgames.com>
2017-11-22 16:40:23 -0800

    Fixed bug with accounts not being able to get added

--- frontend/source/javascript/components/accounts/add.js
+++ frontend/source/javascript/components/accounts/add.js
@@ -41,7 +41,8 @@ function AccountAddController(Utils) {
         if (vm.account.contacts.length === 0) {
             Utils.toast('Contacts cannot be empty', 'error');
         } else {
-            vm.onAccountCreate(vm.account, onAddSuccess, onAddFailure);
+	    const accountInfo = Object.assign({}, vm.account, {enabled: vm.account.enabled ? 1 : 0});
+            vm.onAccountCreate(accountInfo, onAddSuccess, onAddFailure);
         }
     }
     //endregion
